e06060fd5e053e5a4b20079b8f88f60307a6ce0d
84z2h <90423456+84z2h@users.noreply.github.com>
2021-12-17 15:37:04 +0100

    up

--- src/slack/model/User.java
+++ src/slack/model/User.java
@@ -1,15 +1,54 @@
 package slack.model;
 
 public class User implements HasId {
-    private String pseudo;
-    private String mdp;
-    private String nom;
-    private String prenom;
-    private String mail;
-
-
+    protected String username;
+    protected String password;
+    private static int id=0;
+    protected String userId;
+    protected String mail;
+    protected String nom;
+    protected String prenom;
+    public User(String username, String mdp, String mail, String nom, String prenom){
+        this.username=username;
+        this.password=mdp;
+        this.mail=mail;
+        this.nom=nom;
+        this.prenom=prenom;
+        userId=String.valueOf(id);
+        id++;
+    }
+    public void setPrenom(String p){
+        this.prenom=p;
+    }
+    public void setNom(String n){
+        this.nom=n;
+    }
+    public void setMail(String m){
+        this.mail=m;
+    }
+    public void setPassword(String m){
+        this.password=m;
+    }
+    public void setUsername(String user){
+        this.username=user;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public String getPrenom(){
+        return prenom;
+    }
+    public String getNom(){
+        return nom;
+    }
+    public String getMail() {
+        return mail;
+    }
     @Override
     public String getId() {
-        return pseudo;
+        return userId;
     }
 }
