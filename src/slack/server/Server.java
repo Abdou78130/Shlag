b351975d84b62ead9df48cbb6e39070e10777df2
84z2h <90423456+84z2h@users.noreply.github.com>
2021-12-18 23:44:45 +0100

    add server

--- src/slack/server/Server.java
+++ src/slack/server/Server.java
@@ -1,24 +1,34 @@
+package slack.server;
+
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
-import java.nio.channels.AsynchronousServerSocketChannel;
-import java.nio.channels.AsynchronousSocketChannel;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
+import java.io.StringWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
-    public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
+
+    public static void main(String[] args) throws IOException {
+        ExecutorService pool = Executors.newCachedThreadPool();
         System.out.println("Creating a server on port 1236");
-        try(AsynchronousServerSocketChannel serverSocket = AsynchronousServerSocketChannel.open()) {
-            serverSocket.bind(new InetSocketAddress(1236));
+        try(ServerSocket serverSocket = new ServerSocket(1236)) {
             System.out.println("Waiting a connection...");
-            Future<AsynchronousSocketChannel> future = serverSocket.accept();
-            AsynchronousSocketChannel socketChannel = future.get();
-            System.out.println("A client is connected from " + socketChannel.getRemoteAddress());
-            ByteBuffer buffer = ByteBuffer.allocate(1024);
-            socketChannel.read(buffer).get();
-            buffer.flip();
-            System.out.println("Client has sent : \n" + new String(buffer.array()));
+            int idClient = 0;
+            while(true) {
+                Socket socket = serverSocket.accept();
+                int myid = ++idClient;
+                pool.submit(() -> {
+                    System.out.println("A client is connected from " + socket.getInetAddress().getHostAddress());
+                    StringWriter writer = new StringWriter();
+                    //IOUtils.copy(socket.getInputStream(), writer, StandardCharsets.UTF_8);
+                    System.out.println("Client " + myid + " has sent : \n" + writer);
+                    return true;
+                });
+            }
+        } finally {
+            pool.shutdown();
         }
     }
 }
