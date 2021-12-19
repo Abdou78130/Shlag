31b4e5e761af1a70878463ad095fff94dac84958
Lucas Mastier <73110352+LucasMastier@users.noreply.github.com>
2021-12-19 10:41:52 +0100

    Setup de scene builder

--- JavaFXModule/src/main/java/com/example/javafxmodule/Controller.java
+++ JavaFXModule/src/main/java/com/example/javafxmodule/Controller.java
@@ -3,7 +3,7 @@ package com.example.javafxmodule;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
