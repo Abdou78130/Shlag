9579798d0609ba019cf0cca4513c1146b496d5cc
Lucas Mastier <73110352+LucasMastier@users.noreply.github.com>
2021-12-26 21:11:08 +0100

    Up

--- JavaFXModule/src/main/java/com/example/javafxmodule/Main.java
+++ JavaFXModule/src/main/java/com/example/javafxmodule/Main.java
@@ -11,7 +11,11 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("sample.fxml"));
+
+        String css = this.getClass().getResource("application.css").toExternalForm();
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        scene.getStylesheets().add(css);
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
