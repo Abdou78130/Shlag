e4bf1ff900e9918a4fc4efcda63b9bead1c6ff22
84z2h <90423456+84z2h@users.noreply.github.com>
2021-12-26 21:53:05 +0100

    Revert "Merge branch 'main' of https://github.com/Abdou78130/Shlag"
    This reverts commit e3f0b4d62a4b198b1b8e543af5a6f255c4269cbd, reversing
    changes made to 990abc5dd9dae6f43c6d0ee9cce157496fe7632e.

--- JavaFXModule/src/main/java/com/example/javafxmodule/Controller.java
+++ JavaFXModule/src/main/java/com/example/javafxmodule/Controller.java
@@ -2,7 +2,6 @@ package com.example.javafxmodule;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.event.ActionEvent;
 
 public class Controller {
     @FXML
@@ -11,10 +10,5 @@ public class Controller {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
-    }
-
-    @FXML
-    protected void pressButton(ActionEvent event){
-        System.out.println("Bang ca flingue");
     }
 }
