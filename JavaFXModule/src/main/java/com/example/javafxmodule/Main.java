f4f41248e0f22226fb09bfe8ac6c51ce2342cb2c
Lucas Mastier <73110352+LucasMastier@users.noreply.github.com>
2021-12-27 22:55:57 +0100

    Started sign up UI

--- JavaFXModule/src/main/java/com/example/javafxmodule/Main.java
+++ JavaFXModule/src/main/java/com/example/javafxmodule/Main.java
@@ -12,9 +12,11 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("sample.fxml"));
 
-        String css = this.getClass().getResource("application.css").toExternalForm();
+        //fxmlLoader.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+        //String css = this.getClass().getResource("application.css").toExternalForm();
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        scene.getStylesheets().add(css);
+        //scene.getStylesheets().add(css);
+        scene.getStylesheets().add("src/main/java/com/example/javafxmodule/application.css");
 
         stage.setTitle("Hello!");
         stage.setScene(scene);
