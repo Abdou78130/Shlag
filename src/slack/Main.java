c72e7900c100e3a87fb32b09bddccc9669c57384
84z2h <90423456+84z2h@users.noreply.github.com>
2022-01-02 17:03:18 +0100

    bug

--- src/slack/Main.java
+++ src/slack/Main.java
@@ -13,8 +13,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        UserService us = new UserService();
-        MessageService ms = new MessageService();
 
         User u = UserService.inscription("saren", "mdp", "saren@gmail.com", "Mastier", "Lucas");
         User u2 = UserService.inscription("abdou78", "mdp", "abdou78@gmail.com", "Haba", "Abdallah");
--- src/slack/server/Server.java
+++ src/slack/server/Server.java
@@ -1,13 +1,15 @@
 package slack.server;
 
 import slack.model.Channel;
+import slack.model.User;
 import slack.service.ChannelService;
 import slack.service.MessageService;
+import slack.service.UserService;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.channels.Channels;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -15,25 +17,39 @@ import java.util.concurrent.Executors;
 public class Server {
     public static void main(String[] args) throws IOException {
         ExecutorService pool = Executors.newCachedThreadPool();
-        ChannelService.creerChannel("#general");
-        Channel general = ChannelService.channelRepository.select("#general");
         System.out.println("Creating a server on port 1236");
         try (ServerSocket serverSocket = new ServerSocket(1236)) {
+            ChannelService.creerChannel("#general");
+            Channel general = ChannelService.channelRepository.select("#general");
+            List<User> list_user = new ArrayList<User>();
+            ChannelService.creerChannel("#ProjetShlag");
+            ChannelService.creerChannel("#Cours");
             while (true) {
                 Socket socket = serverSocket.accept();
                 pool.submit(() -> {
                     BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                     PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
                     String ligne = reader.readLine();
+                    list_user.add(UserService.userRepository.select(ligne));
+                    User current_user=UserService.userRepository.select(ligne);
                     String reponse = ligne + " s'est connecté au Channel : " + general.getId() + " !";
                     writer.println(reponse);
+                    String chan= null;
                     boolean t = true;
                     while (t) {
                         ligne = reader.readLine();
                         reponse = ligne;
+/*
+                        chan=ligne.substring(7);
+                        if(("/join "+ChannelService.channelRepository.select(chan)).equals(reponse)){
+                            general=ChannelService.channelRepository.select(chan);
+                            System.out.println(chan);
+                            break;
+                        }
+ */
                         System.out.println(reponse);
                         writer.println(reponse);
-                        // ms.creerMessage(reponse,general,); pas fini
+                        MessageService.creerMessage(reponse,general,current_user);
                     }
                     return true;
                 });
@@ -41,5 +57,8 @@ public class Server {
         } finally {
             pool.shutdown();
         }
+    }
+    public static void connectionAccept(){
+
     }
 }
