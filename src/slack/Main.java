ed0ee6ac31632fc9b81b28b8ecc9efe04714a9e8
84z2h <90423456+84z2h@users.noreply.github.com>
2022-01-02 18:22:10 +0100

    up

--- src/slack/Main.java
+++ src/slack/Main.java
@@ -1,17 +1,24 @@
 package slack;
 
+import slack.model.Message;
+import slack.model.User;
+import slack.server.Client;
+import slack.service.MessageService;
+import slack.service.UserService;
+
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        /*
+
         User u = UserService.inscription("saren", "mdp", "saren@gmail.com", "Mastier", "Lucas");
         User u2 = UserService.inscription("abdou78", "mdp", "abdou78@gmail.com", "Haba", "Abdallah");
         List<User> list = UserService.userRepository.select();
         List<Message> list_mess = MessageService.messageRepository.select();
 
-        if (UserService.authenticate(u.getId(), u.getPassword())!=null) {
+        if (UserService.authenticate(u.getId(), u.getPassword()) != null) {
             Client.connectionServer(UserService.userRepository.select(u.getId()));
-        }*/
+        }
     }
 }
--- src/slack/server/Server.java
+++ src/slack/server/Server.java
@@ -19,19 +19,22 @@ public class Server {
         ExecutorService pool = Executors.newCachedThreadPool();
         System.out.println("Creating a server on port 1236");
         try (ServerSocket serverSocket = new ServerSocket(1236)) {
-            ChannelService.creerChannel("#general");
-            Channel general = ChannelService.channelRepository.select("#general");
-            List<User> list_user = new ArrayList<User>();
-            ChannelService.creerChannel("#ProjetShlag");
-            ChannelService.creerChannel("#Cours");
             while (true) {
+                ChannelService.creerChannel("#general");
+                Channel general = ChannelService.channelRepository.select("#general");
+                List<String> list_user = new ArrayList<String>();
+                ChannelService.creerChannel("#ProjetShlag");
+                ChannelService.creerChannel("#Cours");
                 Socket socket = serverSocket.accept();
+
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+
                 pool.submit(() -> {
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
                     String ligne = reader.readLine();
-                    list_user.add(UserService.userRepository.select(ligne));
-                    User current_user=UserService.userRepository.select(ligne);
+                    list_user.add(ligne);
+                    String cur_user=ligne;
                     String reponse = ligne + " s'est connecté au Channel : " + general.getId() + " !";
                     writer.println(reponse);
                     String chan= null;
@@ -39,17 +42,23 @@ public class Server {
                     while (t) {
                         ligne = reader.readLine();
                         reponse = ligne;
-/*
+                        /* join channel PROBLEME
                         chan=ligne.substring(7); //On obtient le nom du channel
                         if(("/join "+ChannelService.channelRepository.select(chan)).equals(reponse)){
                             general=ChannelService.channelRepository.select(chan);
                             System.out.println(chan);
                             break;
                         }
- */
+                        */
                         System.out.println(reponse);
                         writer.println(reponse);
-                        MessageService.creerMessage(reponse,general,current_user);
+                        for (User u: UserService.userRepository.select()) {
+                            if(u.getId().equals(cur_user)){
+                                MessageService.creerMessage(reponse,general,u);
+                                System.out.println("message bien creer");
+                                break;
+                            }
+                        }
                     }
                     return true;
                 });
