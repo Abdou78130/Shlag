0bbab5d3f0dd57996af405695bff25ea613f65df
Lucas Mastier <73110352+LucasMastier@users.noreply.github.com>
2022-01-02 17:26:36 +0100

    Fix

--- src/slack/Main.java
+++ src/slack/Main.java
@@ -1,17 +1,10 @@
 package slack;
 
-import slack.model.Message;
-import slack.model.User;
-import slack.server.Client;
-import slack.service.MessageService;
-import slack.service.UserService;
-
 import java.io.IOException;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
+        /*
         User u = UserService.inscription("saren", "mdp", "saren@gmail.com", "Mastier", "Lucas");
         User u2 = UserService.inscription("abdou78", "mdp", "abdou78@gmail.com", "Haba", "Abdallah");
         List<User> list = UserService.userRepository.select();
@@ -19,6 +12,6 @@ public class Main {
 
         if (UserService.authenticate(u.getId(), u.getPassword())) {
             Client.connectionServer(UserService.userRepository.select(u.getId()));
-        }
+        }*/
     }
 }
