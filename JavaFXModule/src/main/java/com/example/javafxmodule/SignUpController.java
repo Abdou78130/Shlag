1fd7dba0c78207e8625c34e1929d9776c9e63e78
Lucas Mastier <73110352+LucasMastier@users.noreply.github.com>
2022-01-03 18:47:11 +0100

    Added sign up feature

--- JavaFXModule/src/main/java/com/example/javafxmodule/SignUpController.java
+++ JavaFXModule/src/main/java/com/example/javafxmodule/SignUpController.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import slack.service.UserService;
 
 import java.io.IOException;
 
@@ -23,6 +24,15 @@ public class SignUpController {
     private TextField passwordInput;
 
     @FXML
+    private TextField usernameInput;
+
+    @FXML
+    private TextField nameInput;
+
+    @FXML
+    private TextField firstNameInput;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
@@ -36,7 +46,7 @@ public class SignUpController {
 
 
     //Switch sur la fenêtre de connexion au click du bouton "Je suis déjà inscrit"
-
+    @FXML
     public void switchToSignIn(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SignUpController.class.getResource("SignIn.fxml"));
 
@@ -46,6 +56,27 @@ public class SignUpController {
 
         stage.setScene(scene);
         stage.show();
+    }
+
+    @FXML
+    public void signUp(ActionEvent event) throws IOException{
+        String email = idInput.getText();
+        String password = passwordInput.getText();
+        String username = usernameInput.getText();
+        String name = nameInput.getText();
+        String firstName = firstNameInput.getText();
+
+        UserService.inscription(username, password, email, name, firstName);
+
+
+        FXMLLoader fxmlLoader = new FXMLLoader(SignUpController.class.getResource("SignIn.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("SignIn.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load(),1920,1080);
+
+        stage.setScene(scene);
+        stage.show();
+
     }
 
 }
