58093dff3c45763e6e68e82dad82b6168ab39cfa
84z2h <90423456+84z2h@users.noreply.github.com>
2022-01-03 19:11:03 +0100

    server up

--- src/slack/server/Server.java
+++ src/slack/server/Server.java
@@ -1,5 +1,6 @@
 package slack.server;
 
+import slack.model.Admin;
 import slack.model.Channel;
 import slack.model.User;
 import slack.service.ChannelService;
@@ -21,40 +22,61 @@ public class Server {
         try (ServerSocket serverSocket = new ServerSocket(1236)) {
             while (true) {
                 ChannelService.creerChannel("#general");
-                Channel general = ChannelService.channelRepository.select("#general");
-                List<String> list_user = new ArrayList<String>();
                 ChannelService.creerChannel("#ProjetShlag");
                 ChannelService.creerChannel("#Cours");
                 Socket socket = serverSocket.accept();
-
-
-                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-
+                User cur_us =UserService.getCurrentUser();
+                Channel cur_chan = ChannelService.channelRepository.select("#general");
+                //cur_chan.addUser(cur_us.getUserId()); //PROBLEME
                 pool.submit(() -> {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
                     String ligne = reader.readLine();
-                    list_user.add(ligne);
                     String cur_user=ligne;
-                    String reponse = ligne + " s'est connecté au Channel : " + general.getId() + " !";
+
+                    //System.out.println(cur_us.getId());
+                    String reponse = ligne + " s'est connecté au Channel : " + cur_chan.getId() + " !";
                     writer.println(reponse);
                     String chan= null;
                     boolean t = true;
                     while (t) {
                         ligne = reader.readLine();
                         reponse = ligne;
-                        /* join channel PROBLEME
+
+                        // join channel PROBLEME
                         chan=ligne.substring(7); //On obtient le nom du channel
                         if(("/join "+ChannelService.channelRepository.select(chan)).equals(reponse)){
-                            general=ChannelService.channelRepository.select(chan);
-                            System.out.println(chan);
-                            break;
+                            //Enlever user du current channel
+                            //cur_chan=ChannelService.channelRepository.select(chan); //PROBLEME
+                            reponse=ChannelService.channelRepository.select(chan).getId() + "s'est connecté au Channel : "+ChannelService.channelRepository.select(chan).getId()+" !";
+                            continue;
                         }
-                        */
+
+                        //CREATION CHANNEL pas test
+                        chan=ligne.substring(0,9);
+                        if((chan).equals("/create ")){
+                            if(cur_us instanceof Admin){
+                                chan=ligne.substring(9); //on prend le nom du channel tapé
+                                ChannelService.creerChannel(chan);
+                                continue;
+                            }
+                        }
+
+                        //CREATION CHANNEL pas test
+                        chan=ligne.substring(0,9);
+                        if((chan).equals("/delete ")){
+                            if(cur_us instanceof Admin){
+                                chan=ligne.substring(9); //on prend le nom du channel tapé
+                                ChannelService.supprimerChannel(chan);
+                                continue;
+                            }
+                        }
                         System.out.println(reponse);
                         writer.println(reponse);
+                        //PROBLEME for
                         for (User u: UserService.userRepository.select()) {
                             if(u.getId().equals(cur_user)){
-                                MessageService.creerMessage(reponse,general,u);
+                                MessageService.creerMessage(reponse,cur_chan,u);
                                 System.out.println("message bien creer");
                                 break;
                             }
