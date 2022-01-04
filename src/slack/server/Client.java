e4c0ba4561ea5c6a6f1db986c96ed414e0a8ac6c
84z2h <90423456+84z2h@users.noreply.github.com>
2022-01-05 00:08:33 +0100

    omg client/server fini ??

--- src/slack/server/Client.java
+++ src/slack/server/Client.java
@@ -26,17 +26,17 @@ public class Client {
             line = null;
 
             User cur_us = UserService.getCurrentUser();
-            Channel cur_chan = ChannelService.channelRepository.select("#general");
-            cur_chan.addUser(cur_us.getUserId());
+            Channel cur_chan = ChannelService.ConnexionChannel("#general");
+            //cur_chan.addUser(cur_us.getUserId());
             do{
                 line=entree.readLine();
                 line = u.getId()+": "+line;
                 writer.println(line);
-                Client.readServ(reader,cur_us);
+                Client.readServ(reader,cur_us,cur_chan);
             }while(line!=null);
         }
     }
-    private static void readServ(BufferedReader reader, User cur_u){
+    private static void readServ(BufferedReader reader, User cur_u, Channel cur_c){
         try {
             String reponse = reader.readLine();
 
@@ -47,25 +47,24 @@ public class Client {
                 cmd = substr[1];
                 chan = substr[2];
             }
-
             // join channel PROBLEME
             if(cmd.equals("/join")){
                 if(ChannelService.channelRepository.select(chan)!=null) {
-                    //Enlever user du current channel
-                    //cur_c=ChannelService.channelRepository.select(chan);
-                    //reponse = ChannelService.channelRepository.select(chan).getId() + "s'est connecté au Channel : " + ChannelService.channelRepository.select(chan).getId() + " !";
+                    //Enlever user ddans l'ancien channel
+                    cur_c=ChannelService.ConnexionChannel(chan);
+                    reponse = cur_u.getId()+ " s'est connecté au Channel : "+cur_c.getId();
                     System.out.println("Changement bien effectué");
                 }
             }
-
-
             // CREATION channel
             if (cmd.equals("/create")){
                 if (cur_u.getAdmin()) {
                     ChannelService.creerChannel(chan);
                     System.out.println("Channel bien créer");
+                    return;
                 } else {
                     System.out.println("Vous n'avez pas les droits nécessaire pour cette commande");
+                    return;
                 }
             }
             // DELETE channel
@@ -73,18 +72,21 @@ public class Client {
                 if (cur_u.getAdmin()) {
                     ChannelService.supprimerChannel(chan);
                     System.out.println("Channel bien supprimer");
+                    return;
                 } else {
                     System.out.println("Vous n'avez pas les droits nécessaire pour cette commande");
+                    return;
                 }
             }
             //Affiche la liste des channels
             if(cmd.equals("/affiche") && chan.equals("listeChannel")){
                 for ( Channel c: ChannelService.channelRepository.select()){
                   System.out.println(c.getId());
+                  return;
                 }
             }
-
             System.out.println(reponse);
+            MessageService.creerMessage(reponse,cur_c,cur_u);
         }catch(IOException e){
             e.printStackTrace();
         }
