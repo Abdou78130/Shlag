1b3958116343f6191ab9b6913c664e7247c213c7
Lucas Mastier <73110352+LucasMastier@users.noreply.github.com>
2022-01-04 23:18:04 +0100

    Update Main.java

--- JavaFXModule/src/main/java/com/example/javafxmodule/Main.java
+++ JavaFXModule/src/main/java/com/example/javafxmodule/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import slack.model.Message;
 import slack.model.User;
+import slack.service.ChannelService;
 import slack.service.MessageService;
 import slack.service.UserService;
 
@@ -24,6 +25,10 @@ public class Main extends Application {
         List<User> list = UserService.userRepository.select();
         List<Message> list_mess = MessageService.messageRepository.select();
 
+        ChannelService.creerChannel("#general");
+        ChannelService.creerChannel("#ProjetShlag");
+        ChannelService.creerChannel("#Cours");
+
 
         //Partie IHM
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("SignUp.fxml"));
@@ -36,7 +41,7 @@ public class Main extends Application {
 
         //URL imageUrl = getClass().getResource("/images/drawIcon.png");
         //Image image = ImageIO.read(url);
-        Image image = new Image("/ShlagLogo.png");
+        Image image = new Image("C:\\Users\\U1\\Documents\\GitHub\\Shlag\\JavaFXModule\\src\\main\\resources\\images\\ShlagLogo.png");
         stage.getIcons().add(image);
         stage.setTitle("Inscription");
         stage.setScene(scene);
